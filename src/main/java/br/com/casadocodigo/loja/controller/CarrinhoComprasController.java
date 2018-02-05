ddee39cba666638da5848b339b3cc9b106b7a7b0
Douglas Mariano Silverio <douglas07@gmail.com>
2018-02-05 00:05:17 -0200

    First Commit. end of course part I

---
+++ src/main/java/br/com/casadocodigo/loja/controller/CarrinhoComprasController.java
@@ -0,0 +1,58 @@
+package br.com.casadocodigo.loja.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContext;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.CarrinhoCompras;
+import br.com.casadocodigo.loja.models.CarrinhoItem;
+import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
+
+@Controller
+@Scope(value = WebApplicationContext.SCOPE_REQUEST)
+@RequestMapping("/carrinho")
+public class CarrinhoComprasController {
+
+	@Autowired
+	CarrinhoCompras carrinho;
+
+	@Autowired
+	ProdutoDAO produtoDAO;
+
+	private CarrinhoItem criaItem(Integer ProdutoId, TipoPreco tipo) {
+		Produto produto = produtoDAO.find(ProdutoId);
+		CarrinhoItem carrinhoItem = new CarrinhoItem(tipo, produto);
+		return carrinhoItem;
+
+	}
+
+	@RequestMapping("/add")
+	public ModelAndView add(Integer produtoId, TipoPreco tipo) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/carrinho");
+		
+		System.out.println(tipo);
+		CarrinhoItem carrinhoItem = criaItem(produtoId, tipo);
+		carrinho.add(carrinhoItem);
+
+		return modelAndView;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView itens() {
+		return new ModelAndView("/carrinho/itens");
+	}
+	
+	@RequestMapping("/remover")
+	public ModelAndView remover(Integer produtoId, TipoPreco tipoPreco){
+	    carrinho.remover(produtoId, tipoPreco);
+	    return new ModelAndView("redirect:/carrinho");
+	}
+
+}
